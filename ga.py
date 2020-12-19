280e52d8b7bffdb5ab15df8a34782f8302209239
ajkadam108 <ajkadam@uchicago.edu>
2020-12-19 16:19:09 -0600

    first commit

---
+++ ga.py
@@ -0,0 +1,25 @@
+#%%
+## Libraries
+import numpy as np
+import pandas as pd 
+import pandasql as ps
+import plotly
+
+
+#%%
+## Functions
+export_table = lambda dataframe, filename: dataframe.to_excel(filename, index=False)
+
+
+## Variables
+file_master = 'dataset.xlsx'
+dir_processed = '2_processed'
+sheets = ['dataset']
+
+#%%
+# Read all data files
+print(f'Reading Master data from: {file_master}')
+for sheet in sheets:
+    print(f'> {sheet}...')
+    #### UN-COMMENT THE EXEC LINE BELOW WHEN YOU WANT TO READ THE DATA
+    exec(f'tbl_{sheet} = pd.read_excel(file_master, sheet_name="{sheet}")')
